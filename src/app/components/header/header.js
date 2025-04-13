81b1238a90012ac071fe170248b34c0e13263d1f
Hawinf <hawinf54@gmail.com>
2025-04-13 17:59:35 +0400

    adding maps in header

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -4,17 +4,60 @@ import './header.css';
 
 export default function Header() {
   return (
-    <div className='wrapper-header'>
-        <header className='header-cp'>
-            <h1>KATSIARYNA</h1>
-            <h5>PUTSILOUSKAYA</h5>
-            <img src='/header-cp5.png' alt='kate' />
-            <div className='head-desc'>
-                <a href='' className='link-blog'>READ MY BLOG</a>
-                <a href='' className='link-video'>VIDEO</a>
-                <p >I create travel guides and backpacking itineraries from around the world, as well as sharing photography resources and more.</p>
-            </div>
-        </header>
+    <div className='container-header'>
+      <div className='wrapper-header'>
+          <header className='header-cp'>
+              <h1>KATSIARYNA</h1>
+              <h5>PUTSILOUSKAYA</h5>
+              <img src='/header-cp5.png' alt='kate' />
+              <div className='head-desc'>
+                  <a href='' className='link-blog'>READ MY BLOG</a>
+                  <a href='' className='link-video'>VIDEO</a>
+                  <p >I create travel guides and backpacking itineraries from around the world, as well as sharing photography resources and more.</p>
+              </div>
+          </header>
+          
+      </div>
+      <div className='main-header'>
+        <div className='continents-map'>
+          <div className='box-map'>
+            <a href=''>
+              <img className='each-map' src='/africa.png' alt='africa' />
+              <p>Africa</p>
+            </a>
+          </div>
+          <div className='box-map'>
+            <a href=''>
+              <img className='each-map' src='/asia.png' alt='asia' />
+              <p>Asia</p>
+            </a>
+          </div>
+          <div className='box-map'>
+            <a href=''>
+              <img className='each-map' src='/europe.png' alt='europe' />
+              <p>Europe</p>
+            </a>
+          </div>
+          <div className='box-map'>
+            <a href=''>
+              <img className='each-map' src='/north-america.png' alt='north-america' />
+              <p>North America</p>
+            </a>
+          </div>
+          <div className='box-map'>
+            <a href=''>
+              <img className='each-map' src='/south-america.png' alt='south-america' />
+              <p>South America</p>
+            </a>
+          </div>
+          <div className='box-map'>
+            <a href=''>
+              <img className='each-map' src='/oceania.png' alt='oceania' />
+              <p>Oceania</p>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
