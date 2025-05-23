247c29e168e30b9fb629fd0bd2b3e8a04fc48874
hawinf <hawinf54@gmail.com>
2025-05-23 16:28:16 +0400

    adding shop section

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -12,7 +12,20 @@ export default function Shop() {
                     <h1>"Get a Stunning Website – Custom Design Services"</h1>
                     <Link href='' className='nav-orderbtn'>Shop</Link>
                 </nav>
+                <main className='shop-section'>
+                    <nav className='shop-branch'>
+                        <h1>Empowering Your Business Online – One Pixel at a Time.</h1>
+                    </nav>
+                    <nav className='shop-branch2'>
+                        <main className='section-branch-top'>
+                            <h1>More Than a Website – It's Your Bridge to the World.</h1>
+                        </main>
+                        <main className='section-branch-down'>
+                            <h1>Engage. Inspire. Convert. Websites That Speak to Your Audience.</h1>
+                        </main>
+                    </nav>
+                </main>
             </main>
         </>
     );
-};
+};``
