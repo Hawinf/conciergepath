c8d07016149d635b797bf401f3746c47bb038c5c
hawinf <hawinf54@gmail.com>
2025-05-24 04:28:21 +0400

    fixing sixe in shop.jd

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -1,4 +1,5 @@
 import './footer.css';
+import Link from 'next/link';
 
 export default function Footer() {
 
@@ -12,18 +13,18 @@ export default function Footer() {
                     <h1>JOIN MY CHANNEL</h1>
                     
                     <div className='footer-social'>
-                        <a href='https://t.me/qatsiaryna' target='blank'>
+                        <Link href='https://t.me/qatsiaryna' target='blank'>
                             <div className='footer-socialbtn'>
                                 <img src='/telegram.png' alt='kate telegram channel' />
                                 <p>TELEGRAM</p>
                             </div>
-                        </a>
-                        <a href='https://www.tiktok.com/@katsia55?_t=ZS-8wGN9551DlA&_r=1' target='blank'>
+                        </Link>
+                        <Link href='https://www.tiktok.com/@katsia55?_t=ZS-8wGN9551DlA&_r=1' target='blank'>
                             <div className='footer-socialbtn1'>
                                 <img src='/tiktok.png' alt='kate telegram channel' />
                                 <p>TIKTOK</p>
                             </div>
-                        </a>
+                        </Link>
                     </div>
                     <h6>Keep up with my newest content </h6>
                 </div>
@@ -59,8 +60,8 @@ export default function Footer() {
                 <div className='section-one'>
                     <h6>Stay Update</h6>
                     <div>
-                        <a href='https://t.me/qatsiaryna' target='blank'><img src='/telegram.png' alt='kate telegram channel' /></a>
-                        <a href='https://www.tiktok.com/@katsia55?_t=ZS-8wGN9551DlA&_r=1' target='blank'><img src='/tiktok.png' alt='kate telegram channel' /></a>
+                        <Link href='https://t.me/qatsiaryna' target='blank'><img src='/telegram.png' alt='kate telegram channel' /></Link>
+                        <Link href='https://www.tiktok.com/@katsia55?_t=ZS-8wGN9551DlA&_r=1' target='blank'><img src='/tiktok.png' alt='kate telegram channel' /></Link>
                     </div>
                     <p>© 2025 Kate. All rights reserved.</p>
                 </div>
